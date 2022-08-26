8240ac58392dcbbd04fe08f9695dfebd038fdd2a
JVelezFD <jvelez117@gmail.com>
2022-08-25 20:04:33 -0400

    corrected the column call for view fields

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const setupQuest = async () => {
 }
 
 const viewEmployees = async () => {
-    let data = await connectDB.promise().query("SELECT a.id AS 'ID', a.first_name AS 'First Name', a.last_name AS 'Last Name', roles.title AS 'Job Title', department.name AS 'Department', roles.salary AS 'Salary', CONCAT(b.first_name, ' ', b.last_name) AS 'Manager' FROM employee a JOIN roles ON a.role_id = roles.id JOIN department ON roles.department_id = department.id LEFT OUTER JOIN employee b ON a.manager_id = b.id;")
+    let data = await connectDB.promise().query("SELECT a.id AS 'ID', a.first_name AS 'First Name', a.last_name AS 'Last Name', roles.title AS 'Job Title', department.name AS 'Department', roles.salary AS 'Salary', CONCAT(b.first_name, ' ', b.last_name) AS 'Manager' FROM employee a JOIN roles ON a.id = roles.id JOIN department ON roles.department_id = department.id LEFT OUTER JOIN employee b ON a.manager_id = b.id;")
     console.table(data[0]);
     };
 
