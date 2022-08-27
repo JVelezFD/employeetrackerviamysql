5921aa58177b1e4ca4870d5ad44805edfeb3d5e4
JVelezFD <jvelez117@gmail.com>
2022-08-27 11:12:54 -0400

    corrected some of the syntax issues in seed file and add role in server.js

--- seed.sql
+++ seed.sql
@@ -5,15 +5,10 @@ INSERT INTO roles (title, salary, department_id)
 VALUES ('Junior Software Engineer', 1000000, 1),
 ('Senior Software Engineer', 250000, 1),
 ('Account Executive', 125000, 2),
-('Sales Director', 185000, 2),
-('VP of Sales', 250000, 2),
 ('Controller', 100000, 3),
-('VP of Finance', 150000, 3),
-('CFO', 190000, 3),
 ('General Counsel', 100000, 4),
-('HR Coordinator', 80000, 5),
-('HR Director', 100000, 5),
-('VP of Human Resources', 150000, 5);
+('HR Coordinator', 80000, 5);
+
 
 
 INSERT INTO employee (first_name, last_name, roles_id, manager_id)
@@ -21,11 +16,5 @@ VALUES ('Jesus', 'Velez', 1, NULL),
 ('Jenny', 'Fromdablock', 2, 1 ),
 ('Benny', 'Fleck', 3, NULL),
 ('Kevin', 'James', 4, 3),
-('Timmy', 'Boyi', 5, NULL),
-('Sean', 'Combs',6, 5),
-('Tina', 'Turner', 7, NULL),
-('Tom', 'Royalcarib', 8, 7),
-('Kevin', 'Hart', 9, NULL),
-('Tobias', 'Fate', 10, 9),
-('Tiffae', 'Baus', 11, NULL),
-('Lilly', 'Pad', 12, 10);
+('Timmy', 'Boyi', 5, NULL);
+
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const setupQuest = async () => {
             name: 'connectionQuest',
             type: 'list',
             message: "You're connected to the employee datbase. What would you like to do?",
-            choices: ['View all employees', 'View all departments', 'View all roles', 'Add a Department', 'Add a role', 'Add an employee role', 'Update an employee']
+            choices: ['View all employees', 'View all departments', 'View all roles', 'Add a Department', 'Add a role', 'Add an employee', 'Update an employee']
 
         },
     ])
@@ -37,11 +37,11 @@ const setupQuest = async () => {
                 addDept()
             }
 
-            if (userData.connectionQuest === "Add a roles") {
+            if (userData.connectionQuest === "Add a role") {
                 addRoles()
             }
 
-            if (userData.connectionQuest === "Add an employee role") {
+            if (userData.connectionQuest === "Add an employee") {
                 addEmployee()
             }
 
@@ -53,9 +53,9 @@ const setupQuest = async () => {
 }
 
 const viewEmployees = async () => {
-    let data = await connectDB.promise().query("SELECT a.id AS 'ID', a.first_name AS 'First Name', a.last_name AS 'Last Name', roles.title AS 'Job Title', department.name AS 'Department', roles.salary AS 'Salary', CONCAT(b.first_name, ' ', b.last_name) AS 'Manager' FROM employee a JOIN roles ON a.id = roles.id JOIN department ON roles.department_id = department.id LEFT OUTER JOIN employee b ON a.manager_id = b.id;")
+    let data = await connectDB.promise().query("SELECT a.id AS 'ID', a.first_name AS 'First Name', a.last_name AS 'Last Name', roles.title AS 'Job Title', department.name AS 'Department', roles.salary AS 'Salary', CONCAT(b.first_name, '  ', b.last_name) AS 'Manager' FROM employee a JOIN roles ON a.id = roles.id JOIN department ON roles.department_id = department.id LEFT OUTER JOIN employee b ON a.manager_id = b.id;")
     console.table(data[0]);
-    };
+};
 
 const viewDepartments = async () => {
     let data = await connectDB.promise().query("SELECT * FROM department");
@@ -78,7 +78,6 @@ const addDept = () => {
         setupQuest()
     })
 };
-
 const addRoles = async () => {
     let departments = await connectDB.promise().query(
         `SELECT name,id AS value FROM department`
@@ -110,6 +109,55 @@ const addRoles = async () => {
         )
     })
 };
+// const addRoles = () => {
+
+//     connectDB.query(`SELECT * FROM department;`, (err, res) => {
+//         if (err) throw err;
+//         let department = res.map(departments => ({ name: department.name, value: department.id }));
+
+//         // return connectDB.promise().query(
+//         //     `SELECT department.id, department.name FROM department;`
+//         // ).then(([rows])=> {
+//         //     let departments = rows;
+//         //         const departChoices = departments.map(({ id, name})=> ({
+//         //     name: name, 
+//         //     value: id 
+//         // }) );
+
+
+
+//         //console.log('departments: ', departments[0]);
+//         inquirer.prompt([
+//             {
+//                 name: 'title',
+//                 type: 'input',
+//                 message: 'Enter the title for the new role'
+//             },
+//             {
+//                 name: 'salary',
+//                 type: 'input',
+//                 message: 'Enter the salary for the new role'
+//             },
+//             {
+//                 name: 'department_id',
+//                 type: 'list',
+//                 message: 'Enter a department id for the new role',
+//                 choices: departChoices
+//             }
+
+//         ]).then((roleInput) => {
+//             return connectDB.query(
+//                 `INSERT INTO roles SET ?`, roleInput
+//                 (err, res) => {
+//                 if (err) throw err;
+//                 setupQuest()
+//             }
+//             )
+      
+//     })
+// })
+
+// };
 
 const addEmployee = () => {
     connectDB.query(`SELECT * FROM roles;`, (err, res) => {
@@ -117,7 +165,7 @@ const addEmployee = () => {
         let role = res.map(roles => ({ name: roles.title, value: roles.id }));
         connectDB.query(`SELECT * FROM employee`, (err, res) => {
             if (err) throw err;
-            let employees = res.map(employee => ({ name: employee.first_name + '' + employee.last_name, value: employee.id }))
+            let employees = res.map(employee => ({ name: employee.first_name + ' ' + employee.last_name, value: employee.id }))
             inquirer.prompt([
                 {
                     name: 'firstName',
@@ -163,7 +211,7 @@ const updateEmployee = () => {
         connectDB.query(`SELECT * FROM employee;`, (err, res) => {
             if (err) throw err;
             let employees = res.map(employee => ({
-                name: employee.first_name + '' + employee.last_name, value: employee_id
+                name: employee.first_name + ' ' + employee.last_name, value: employee.id
             }));
             inquirer.prompt([
                 {
